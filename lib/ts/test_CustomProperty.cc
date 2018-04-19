e495e5c6322ce9b7124911a24ed1f1f273e64755
mynydd <paul.storey@clearswift.com>
2018-04-19 10:40:01 +0100

    Unit tests for addition, retrieval and value destruction.

--- lib/ts/test_CustomProperty.cc
+++ lib/ts/test_CustomProperty.cc
@@ -28,13 +28,92 @@ void intDelete(void*f)
   delete (int*)f;
 }
 
-int
-main()
+class MyValue
 {
-  CustomProperties cp;
-  int* i = new int(45);
-  cp.add("name", (void*)i, intDelete);
-  cp.remove("name");
+public:
+  MyValue(char c = '\0') : m_c(c) {}
+  ~MyValue() {}
+  bool operator==(const MyValue& other) { return this->m_c == other.m_c; }
+  bool operator!=(const MyValue& other) { return !( *this == other);     }
+  static int constructed;
+  static int destroyed;
+private:
+  char m_c;
+  MyValue(const MyValue&);
+  MyValue& operator=(const MyValue&);
+};
+
+
+int destroyedByMap = 0;
+
+
+void resetCounts() { 
+  destroyedByMap = 0;
+}
+
+void destroyMyValue(void* mv) { 
+  if (mv != nullptr) {
+    delete static_cast<MyValue*>(mv); 
+    destroyedByMap++;
+  }
+}
+
+void verify(bool condition, const char* errorMsg = "") { 
+  if (!condition) { 
+    printf("test_CustomProperty FAILED: %s\n", errorMsg); 
+    exit(1); 
+  }   
+}
+
+int
+main() 
+{
+  // Test expectations for addition and retrieval
+  {
+    CustomProperties cp;
+    cp.add("a", new MyValue('a'), destroyMyValue);
+    cp.add("b", new MyValue('b'), destroyMyValue);
+    cp.add("a", new MyValue('x'), destroyMyValue);
+    verify( cp.get("a") != nullptr );
+    verify( cp.get("b") != nullptr );
+    verify( cp.get("c") == nullptr );
+    verify( (*static_cast<MyValue*>(cp.get("a"))) == MyValue('x'));
+    verify( (*static_cast<MyValue*>(cp.get("b"))) == MyValue('b'));
+    verify( (*static_cast<MyValue*>(cp.get("b"))) != MyValue('c'));
+    verify(destroyedByMap == 1,"Expect deletion for repeated insertion using same key");
+  }
+
+  resetCounts();
+
+  // Test expectations for destruction
+  {
+    CustomProperties cp;
+    cp.add("a", new MyValue(), destroyMyValue);
+    cp.add("b", new MyValue(), destroyMyValue);
+    cp.add("c", new MyValue(), destroyMyValue);
+    verify(destroyedByMap == 0);
+  }
+
+  verify(destroyedByMap == 3);
+
+  resetCounts();
+
+  // Test expectations for value removal
+  {
+    CustomProperties cp;
+    cp.add("a", new MyValue('a'), destroyMyValue);
+    cp.add("b", new MyValue('b'), destroyMyValue);
+    cp.add("c", new MyValue('c'), destroyMyValue);
+    cp.remove("a");
+    cp.remove("a");
+    cp.remove("b");
+    verify(destroyedByMap == 2);
+  }
+
+  verify(destroyedByMap == 3);
+
   printf("test_CustomProperty PASSED\n");
+
   exit(0);
 }
+
